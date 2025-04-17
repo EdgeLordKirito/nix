5658872b052ac9640acf37356ea21118812f67ce
EdgeLordKirito <Leiteritz.till@gmail.com>
2025-04-17 21:25:50 +0200

    Init

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Entry Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs = { nixpkgs, nixpkgs-stable, ... } @ inputs: {
+  	nixosConfigurations = {
+	  nixos = let
+	    system = "x86_64-linux";
+
+	    pkgs = import nixpkgs {
+	      inherit system;
+	      config.allowUnfree = true;
+	    };
+
+	    pkgs-stable = import nixpkgs-stable {
+	      inherit system;
+	      config.allowUnfree = true;
+	    };
+
+	  in nixpkgs.lib.nixosSystem {
+	    inherit system;
+	    specialArgs = {
+	      inherit pkgs pkgs-stable;
+	    };
+	    modules = [
+	       ./hosts/nixos
+	    ];
+	  };
+
+      #  Desktop = let
+      #	    system = "x86_64-linux";
+      #
+      #    pkgs = import nixpkgs {
+      #      inherit system;
+      #      config.allowUnfree = true;
+      #    };
+      #
+      #    pkgs-stable = import nixpkgs-stable {
+      #      inherit system;
+      #      config.allowUnfree = true;
+      #    };
+      #
+      #  in nixpkgs.lib.nixosSystem {
+      #    inherit system;
+      #    specialArgs = {
+      #      inherit pkgs pkgs-stable;
+      #    };
+      #    modules = [
+      #      ./hosts/Desktop
+      #    ];
+      #  };
+      	  
+      	};
+  };
+}
---
+++ hosts/nixos/default.nix
@@ -0,0 +1,45 @@
+{ ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../modules/configuration.nix
+    ];
+
+  # Bootloader
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub =
+    {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+      useOSProber = false; # enable if dual booting
+      extraEntries = "
+        menuentry \"Windows\" --class windows --class os {\n
+          insmod ntfs\n
+          search --no-floppy --set=root --fs-uuid C236-4D6C\n
+          chainloader /efi/Microsoft/Boot/bootmgfw.efi\n
+        }";
+    };
+
+  networking.hostName = "nixos";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # enables support for Bluetooth
+  hardware.bluetooth.enable = true;
+  # powers up the default Bluetooth controller on boot
+  hardware.bluetooth.powerOnBoot = true;
+  # allow reading device charge
+  hardware.bluetooth.settings = {
+    General = {
+      Experimental = true;
+    };
+  };
+
+  system.stateVersion = "24.11";
+}
