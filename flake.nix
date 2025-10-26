090a7faed70c792442a092cc30a9be1ad143f64e
EdgeLordKirito <Leiteritz.till@gmail.com>
2025-10-26 22:11:09 +0100

    changed hostname

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   outputs = { nixpkgs, nixpkgs-stable, ... } @ inputs: {
   	nixosConfigurations = {
-	  nixos = let
+	  rotom = let
 	    system = "x86_64-linux";
 
 	    pkgs = import nixpkgs {
@@ -29,31 +29,7 @@
 	    modules = [
 	       ./hosts/nixos
 	    ];
-	  };
-
-      #  Desktop = let
-      #	    system = "x86_64-linux";
-      #
-      #    pkgs = import nixpkgs {
-      #      inherit system;
-      #      config.allowUnfree = true;
-      #    };
-      #
-      #    pkgs-stable = import nixpkgs-stable {
-      #      inherit system;
-      #      config.allowUnfree = true;
-      #    };
-      #
-      #  in nixpkgs.lib.nixosSystem {
-      #    inherit system;
-      #    specialArgs = {
-      #      inherit pkgs pkgs-stable;
-      #    };
-      #    modules = [
-      #      ./hosts/Desktop
-      #    ];
-      #  };
-      	  
+	  }; 
       	};
   };
 }
