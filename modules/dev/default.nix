841fe5941e06775d0fab7dd92857e770949c47bd
EdgeLordKirito <Leiteritz.till@gmail.com>
2026-04-05 17:37:16 +0200

    removed javaSdks config from default.nix for per system config

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -6,22 +6,6 @@
 }:
 {
   imports = [
-    ./javaSdks.nix
     ./dev.nix
   ];
-
-  programs.javaSdks = {
-    enable = true;
-
-    homeJavaPackage = pkgs.jdk17;
-
-    extraJavaPackages = [
-      pkgs.jdk8
-      pkgs.jdk11
-      pkgs.jdk21
-    ];
-
-    createEnvVars = true;
-    createAliases = true;
-  };
 }
